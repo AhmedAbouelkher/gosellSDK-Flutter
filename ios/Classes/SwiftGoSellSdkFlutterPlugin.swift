d4aa94c0fd4b155d113c185c2d1b760316d80f40
AhmedAbouelkher <am303737@gmail.com>
2021-09-29 18:38:37 +0200

    empty commit

--- ios/Classes/SwiftGoSellSdkFlutterPlugin.swift
+++ ios/Classes/SwiftGoSellSdkFlutterPlugin.swift
@@ -505,7 +505,7 @@ extension SwiftGoSellSdkFlutterPlugin: SessionDelegate {
              resultMap["card_first_six"] = card.firstSixDigits
              resultMap["card_last_four"] = card.lastFourDigits
              resultMap["card_object"] = card.object
- //            let cardBrand = CardBrand(rawValue: card.brand.rawValue)
+            // let cardBrand = CardBrand(rawValue: card.brand.rawValue)
              resultMap["card_brand"] = card.brand.textValue
              resultMap["card_exp_month"] = card.expirationMonth
              resultMap["card_exp_year"] = card.expirationYear
